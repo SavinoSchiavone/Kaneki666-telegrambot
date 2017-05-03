9dbc13fa12d46f030703bd8ccf0662fb86290061
SavinoSchiavone <savino.schiavone1@gmail.com>
2017-05-03 15:23:11 +0200

    Started group manager project
    Added /startg and the new object $php for the class phpfunc

--- bot.php
+++ bot.php
@@ -9,17 +9,18 @@ require "base.php";
 mysql_select_db("YOUR_DATABASE_NAME");
 
 //bot object
-$telegram = new Telegram("INSER_APIKEY_HERE");
-
-//message
-//$message = $telegram->getMessage();
+$telegram = new Telegram("YOUR_BOT_API_KEY");
 
 //minecraft object
 require_once "minecraft.php";
 $minecraft = new Minecraft();
 
+//phpfunc object
+require_once "phpfunc.php";
+$php = new phpfunc();
+
 //tables
-$table = "DATABSE_TABLE";
+$table = "YOUR_DATABASE_TABLE_NAME";
 
 //database variables
 $select = mysql_query("select * from $table where user_id = '$user_id'");
@@ -28,6 +29,10 @@ $state = $select_result["state"];
 
 //commands array
 $commands = ["/start", "Cancel", "Contact me", "Minecraft Info", "/premium"];
+//start_command array
+$prefix_command = ["!", "/", ".", "#"];
+$start_command = ["!startg", ".startg", "/startg", "#startg"];
+$start_command_ex = ["qualcosa", "qualcosa2"];
 
 //start message
 if ($telegram->getMessage() == "/start") {
@@ -142,6 +147,97 @@ if ($telegram->getMessage() == "Minecraft Info") {
 	$telegram->sendMessage($minecraft->mojangInfo());
 }
 
+
+//group manager
+
+if ($php->startsWith($telegram->getMessage(), $prefix_command, 1) == true) {
+
+$message = str_replace(substr($telegram->getMessage(), 0, 1), "", $telegram->getMessage());	
+$ex = explode(" ", $message);
+
+
+
+if ($ex[0] == "startg") {
+	if ($data == null) {
+		if ($chat_type == "private") {
+			$text = "Welcome to this new GroupManagerBot.
+			Bot made by @Kaneki666
+			You can find the bot source code at github.com/SavinoSchiavone
+			/help for a command list";
+			$button1 = [
+				"text" => "My github :D",
+				"url" => "https://www.github.com/SavinoSchiavone"
+			];
+			$button2 = [
+				"text" => "HELP",
+				"callback_data" => "help"
+			];
+			$structure = [
+				[$button1],
+				[$button2]
+			];
+			$menu = ["inline_keyboard" => $structure];
+			$telegram->sendMessage($text, $menu);
+		} else if ($chat_type == "group" or $chat_type == "supergroup") {
+			$telegram->sendMessage("<b>Contact me in private chat</b>");
+				$text = "Welcome to this new GroupManagerBot.
+			Bot made by @Kaneki666
+			You can find the bot source code at github.com/SavinoSchiavone
+			/help for a commands list";
+			$button1 = [
+				"text" => "My github :D",
+				"url" => "https://www.github.com/SavinoSchiavone"
+			];
+			$button2 = [
+				"text" => "HELP",
+				"callback_data" => "help"
+			];
+			$structure = [
+				[$button1],
+				[$button2]
+			];
+			$menu = ["inline_keyboard" => $structure];
+			$telegram->sendMessage($text, $menu, $user_id);
+		}
+	} else if (in_array($data, $start_command_ex)) {
+		if ($data == "qualcosa") {
+			$telegram->sendMessage("qualcosa");
+		} else if ($data == "qualcosa2") {
+			$telegram->sendMessage("qualcosa2");
+		}
+	} elseif (!in_array($data, $start_command_ex) && $data != null) {
+		$telegram->sendMessage("No riconosciuto");
+	}
+} 
+
+
+/*
+if () {
+	$text = "Commands list
+
+Admins's commands:
+	/ban - Ban an user
+	/unban - Unban an user
+	/kick - Kick an user
+	/info - Get user info
+	/settings - Open setting menu
+
+Users's commands:
+	/info - Get your info
+	/link - Get group link if setted
+	@admin - Alert to all admins
+
+More commands are coming.
+Use /help {command} to see help page for a command
+You can use all /commands also with .command, !command or #command";
+	$telegram->sendMessage($text);
+}
+
+*/
+
+
+
+}//prefix control end
 
 
 ?>
